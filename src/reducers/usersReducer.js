042f383fc5effa7ab4588b2705e5c75bb1633339
Ivan Razum <ivanrazum1611@gmail.com>
2022-05-26 18:04:33 +0200

    Reducers syntax changes

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,8 +1,8 @@
-export default (state = [], action) => {
+export default function usersReducer(state = [], action) {
 	switch (action.type) {
 		case "FETCH_USER":
 			return [...state, action.payload];
 		default:
 			return state;
 	}
-};
+}
